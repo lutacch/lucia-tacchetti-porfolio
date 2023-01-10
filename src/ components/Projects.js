7e67f034b954584e4f5d14a44dd93b934f7a8325
lutacch <lu.tacchetti@gmail.com>
2023-01-10 17:17:16 +0000

    commit

--- src/ components/Projects.js
+++ src/ components/Projects.js
@@ -62,7 +62,7 @@ const Projects = () => {
                 Movies App
               </span>
               <div className="pt-8 text-center">
-                <a href="https://movie-app-rho-indol.vercel.app/">
+                <a href="https://movie-app-project-ten.vercel.app/">
                   <button
                     className="text-center rounded-lg px-4 py-3 m-2
                        bg-white text-gray-700 font-bold text-lg"
